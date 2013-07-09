f428096dc38911c724fb69d7c6233c7360f679a8
finchman <finchman@gmail.com>
2013-07-09 13:15:42 -0400

    ready to add new facebook icon

--- wp-content/plugins/buddypress/bp-themes/bp-default/header.php
+++ wp-content/plugins/buddypress/bp-themes/bp-default/header.php
@@ -11,6 +11,7 @@ echo $nowie;*/
 	<head profile="http://gmpg.org/xfn/11">
 		<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 		<?php if ( current_theme_supports( 'bp-default-responsive' ) ) : ?><meta name="viewport" content="width=device-width, initial-scale=1.0" /><?php endif; ?>
+		<!-- <meta property=”og:image” content=”http://www.yourwebsite.com/images/fbthumb.png” /> --> 
 		<title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
