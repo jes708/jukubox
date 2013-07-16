8c87b5d2fef9672ca7e023172527b97698ecc002
Jon Schwarz <jonschwarz@optonline.net>
2013-07-16 17:42:25 -0400

    added more jschwarz files post email

--- wp-content/plugins/buddypress/bp-themes/bp-default/header.php
+++ wp-content/plugins/buddypress/bp-themes/bp-default/header.php
@@ -100,16 +100,21 @@ echo '</pre>'; */
 				<div class="padder">
 					<h1 id="logo" role="banner"><a href="<?php echo home_url(); ?>" title="<?php _ex( 'Home', 'Home page banner link title', 'buddypress' ); ?>"><img id="jukulogo" src="<?php echo get_home_url(); ?>/wp-content/uploads/2013/07/Beta.png" /><?php /*bp_site_name();*/  ?></a></h1>
 
-						<form action="<?php echo bp_search_form_action(); ?>" method="post" id="search-form">
+						<form class="form-search" action="<?php echo bp_search_form_action(); ?>" method="post" id="search-form">
+<div class="input-append">
 							<label for="search-terms" class="accessibly-hidden"><?php _e( 'Search for:', 'buddypress' ); ?></label>
-							<input type="text" id="search-terms" name="search-terms" value="<?php echo isset( $_REQUEST['s'] ) ? esc_attr( $_REQUEST['s'] ) : ''; ?>" />
+
+							<input class="span3 search-query" type="text" id="search-terms" name="search-terms" value="<?php echo isset( $_REQUEST['s'] ) ? esc_attr( $_REQUEST['s'] ) : ''; ?>" />
+
+
+
+							<button type="submit" class="btn btn-primary" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ); ?>" >
+<i class="icon-search icon-white"></i>
+</button>
 
 							<?php echo bp_search_form_type_select(); ?>
-
-							<input type="submit" name="search-submit" id="search-submit" value="<?php _e( 'Search', 'buddypress' ); ?>" />
-
 							<?php wp_nonce_field( 'bp_search_form' ); ?>
-
+</div>
 						</form><!-- #search-form -->
 
 				<?php do_action( 'bp_search_login_bar' ); ?>
