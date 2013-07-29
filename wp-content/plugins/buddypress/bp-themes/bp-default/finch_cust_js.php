ab916cf3b5ed2fbcc05a45305851a19b54ad250d
Jon Schwarz <jonschwarz@optonline.net>
2013-07-29 19:16:17 -0400

    fixed fonts and made buttons small on teacher list

--- wp-content/plugins/buddypress/bp-themes/bp-default/finch_cust_js.php
+++ wp-content/plugins/buddypress/bp-themes/bp-default/finch_cust_js.php
@@ -419,5 +419,9 @@ jQuery('#serviceForm').ready(function() {
 	jQuery(document).ready(function() {
 		jQuery('.generic-button a').addClass('btn');
 		jQuery('.generic-button a').addClass('btn-primary');
+		jQuery('#members-list .generic-button a').addClass('btn-small');
 });
+
 </script>
+
+
