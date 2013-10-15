f721b7a9a7721a1f02ec17bfd0c8fe6b755b88a9
Jon Schwarz <jonschwarz@optonline.net>
2013-10-15 18:12:02 -0400

    corrected footer syntax

--- wp-content/plugins/buddypress/bp-themes/bp-default/footer.php
+++ wp-content/plugins/buddypress/bp-themes/bp-default/footer.php
@@ -93,8 +93,7 @@ $foot_col_4 = array_slice($instruments_array, 28, 14);
                                         <dl>
                                         <dt value="<?php echo $first_label; ?>"><?php echo $first_label; ?></dt>
                                                 <?php endif; ?>
-					<a href="<?php echo get_home_url() . '/teachers/?instrument=' . $value['name'] ?>"><dd value="<?php echo $value['name']; ?>"><?php echo $value['name']; ?></dd></a>                                       
- 					<?php $last_label = $begin_end['lasts'];
+                                         <dd value="<?php echo $value['name']; ?>"><a href="<?php echo get_home_url() . '/teachers/?instrument=' . $value['name'] ?>"><?php echo $value['name']; ?></a></dd>						<?php $last_label = $begin_end['lasts'];
                                                 if(!empty( $last_label[$instrument_name] ) ) : ?>
                                         </dl>
                                                 <?php endif; ?>
@@ -108,8 +107,8 @@ $foot_col_4 = array_slice($instruments_array, 28, 14);
                                         <dl>
                                         <dt value="<?php echo $first_label; ?>"><?php echo $first_label; ?></dt>
                                                 <?php endif; ?>
-                                 <a href="<?php echo get_home_url() . '/teachers/?instrument=' . $value['name'] ?>"><dd value="<?php echo $value['name']; ?>"><?php echo $value['name']; ?></dd></a>
-                                        <?php $last_label = $begin_end['lasts'];
+                                                <dd value="<?php echo $value['name']; ?>"><a href="<?php echo get_home_url() . '/teachers/?instrument=' . $value['name'] ?>"><?php echo $value['name']; ?></a></dd>
+					  <?php $last_label = $begin_end['lasts'];
                                                 if(!empty( $last_label[$instrument_name] ) ) : ?>
                                         </dl>
                                                 <?php endif; ?>
@@ -123,8 +122,8 @@ $foot_col_4 = array_slice($instruments_array, 28, 14);
                                         <dl>
                                         <dt value="<?php echo $first_label; ?>"><?php echo $first_label; ?></dt>
                                                 <?php endif; ?>
-                                       <a href="<?php echo get_home_url() . '/teachers/?instrument=' . $value['name'] ?>"><dd value="<?php echo $value['name']; ?>"><?php echo $value['name']; ?></dd></a>
-					 <?php $last_label = $begin_end['lasts'];
+					<dd value="<?php echo $value['name']; ?>"><a href="<?php echo get_home_url() . '/teachers/?instrument=' . $value['name'] ?>"><?php echo $value['name']; ?></a></dd> 
+					<?php $last_label = $begin_end['lasts'];
                                                 if(!empty( $last_label[$instrument_name] ) ) : ?>
                                         </dl>
                                                 <?php endif; ?>
@@ -138,7 +137,7 @@ $foot_col_4 = array_slice($instruments_array, 28, 14);
                                         <?php  $instrument_name = $value['name'];
                                                $first_label = $begin_end['firsts'][$instrument_name]; ?>
                                         <ul>
-					<a href="<?php echo get_home_url() . '/teachers/?instrument=' . $value['name'] ?>"><li value="<?php echo $value['name']; ?>"><?php echo $value['name']; ?></li></a>
+					<li value="<?php echo $value['name']; ?>"><a href="<?php echo get_home_url() . '/teachers/?instrument=' . $value['name'] ?>"><?php echo $value['name']; ?></a></li>
                                         <?php $last_label = $begin_end['lasts'];
                                                 if(!empty( $last_label[$instrument_name] ) ) : ?>
                                         </ul>
