9701803e108de776bf4acd67acdad3201d57cc13
finchman <finchman@gmail.com>
2013-11-19 18:29:51 -0500

    formatted index page

--- wp-content/plugins/buddypress/bp-themes/bp-default/index.php
+++ wp-content/plugins/buddypress/bp-themes/bp-default/index.php
@@ -19,22 +19,46 @@
 
 					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
+<h2 class="posttitle"><a href="<?php the_permalink(); ?>" ><!-- <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'buddypress' ); ?>--><?php the_title(); ?></a></h2>
+                        <!--<span class="alignright" style="margin-top: -34px;"><?php /*echo get_avatar( get_the_author_meta( 'user_email' ), '50' );*/ ?></span>-->
+
+
+                        <p class="date">
+                                <?php printf( _x( 'by %s', 'Post written by...', 'buddypress' ), str_replace( '<a href=', '<a rel="author" href=', bp_core_get_userlink( $post->post_author ) ) ); ?> &middot; <?php printf( __( '%1$s <!--<span>in %2$s</span>-->', 'buddypress' ), get_the_date(), get_the_category_list( ', ' ) ); ?> &middot; <?php comments_popup_link( __( 'No Comments', 'buddypress' ), __( '1 Comment', 'buddypress' ), __( '% Comments', 'buddypress' ) ); ?>
+
+
+                                <span class="post-utility alignright"><?php edit_post_link( __( 'Edit this entry', 'buddypress' ) ); ?></span>
+                        </p>
+                        <div class="finchFeatureImg">
+                                <a href="<?php the_permalink(); ?>">            
+                                <?php
+                                        if( has_post_thumbnail() ) {
+                                                the_post_thumbnail('juku-thumb');
+                                        }
+
+                                ?>
+                                </a>
+                        </div><!-- end align center -->
+                                <?php $userlink =  bp_core_get_user_domain( $post->post_author ) . "profile/" ; /*echo $userlink;*/   ?>
+
+<!--
+
 						<div class="author-box">
 							<?php //echo get_avatar( get_the_author_meta( 'user_email' ), '50' ); ?>
-							<p><?php printf( _x( 'by %s', 'Post written by...', 'buddypress' ), bp_core_get_userlink( $post->post_author ) ); ?></p>
+							<p><?php //printf( _x( 'by %s', 'Post written by...', 'buddypress' ), bp_core_get_userlink( $post->post_author ) ); ?></p>
 
-							<?php if ( is_sticky() ) : ?>
-								<span class="activity sticky-post"><?php _ex( 'Featured', 'Sticky post', 'buddypress' ); ?></span>
-							<?php endif; ?>
+							<?php //if ( is_sticky() ) : ?>
+								<span class="activity sticky-post"><?php //_ex( 'Featured', 'Sticky post', 'buddypress' ); ?></span>
+							<?php //endif; ?>
 						</div>
-
+-->
 						<div class="post-content">
-							<h2 class="posttitle"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'buddypress' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+<!--							<h2 class="posttitle"><a href="<?php //the_permalink(); ?>" rel="bookmark" title="<?php //_e( 'Permanent Link to', 'buddypress' ); ?> <?php //the_title_attribute(); ?>"><?php //the_title(); ?></a></h2>
 
-							<p class="date"><?php printf( __( '%1$s <span>in %2$s</span>', 'buddypress' ), get_the_date(), get_the_category_list( ', ' ) ); ?></p>
-
+							<p class="date"><?php //printf( __( '%1$s <span>in %2$s</span>', 'buddypress' ), get_the_date(), get_the_category_list( ', ' ) ); ?></p>
+-->
 							<div class="entry">
-								<?php the_content( __( 'Read the rest of this entry &rarr;', 'buddypress' ) ); ?>
+								<?php the_excerpt( __( 'Read the rest of this entry &rarr;', 'buddypress' ) ); ?>
 								<?php wp_link_pages( array( 'before' => '<div class="page-link"><p>' . __( 'Pages: ', 'buddypress' ), 'after' => '</p></div>', 'next_or_number' => 'number' ) ); ?>
 							</div>
 
